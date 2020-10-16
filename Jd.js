49a96778de4b3af8546695d26b2c2310c5533574
ihechikara <54513385+ihechikara@users.noreply.github.com>
2020-10-16 12:08:48 +0100

    Add files via upload

---
+++ Jd.js
@@ -0,0 +1,23 @@
+/*This code diaplays greetings to your visitors based on the current time of the day*/
+
+var currentDay = new Date(); //creates a new date object
+
+var hourNow = currentDay.getHours(); //gets current hour
+
+var greeting;
+
+/*For the code below: If the current hour is above 18 then greeting will be Good evening; greater than 12 and it'll be Good Afternoon. If greater than zero, it'll be Giod morning*/
+
+if (hourNow > 18) {
+  greeting = "Good Evening! Welcome to my Portfolio!"
+} else if (hourNow > 12) {
+  greeting = "Good Afternoon! Welcome to my Portfolio!"
+} else if (hourNow > 0) {
+  greeting = "Good Morning! Welcome to my Portfolio!"
+} else {
+  greeting = "Welcome! Welcome to my Portfolio!"
+}
+
+document.write("<h1>" + greeting + "<h1>")
+
+/*created with the help of Jon Duckett's JavaScript book*/
